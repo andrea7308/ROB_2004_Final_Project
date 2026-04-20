ee0e20ed20b6cb10731618bed6592282e223e99c
andrea <ag8754@nyu.edu>
2026-04-20 15:56:27 -0400

    added bringup pkg that includes main launch file from lab 3 with our custom nodes

---
+++ bringup/setup.py
@@ -0,0 +1,32 @@
+from setuptools import find_packages, setup
+import os
+from glob import glob
+
+package_name = 'bringup'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join("share", package_name), glob("launch/*.launch.py")),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='andrea',
+    maintainer_email='ag8754@nyu.edu',
+    description='TODO: Package description',
+    license='Apache-2.0',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
