5595c00cd1711102ecd6e6c958d7587a0ae90b90
Andrea Gonzalez <andrea7308.github.io>
2026-04-27 15:58:45 -0400

    included bounding box size

--- ball_tracker/ball_tracker/tracking_controller.py
+++ ball_tracker/ball_tracker/tracking_controller.py
@@ -3,11 +3,13 @@ from rclpy.node import Node
 from std_msgs.msg import String
 from enum import Enum, auto
 from geometry_msgs.msg import Twist
+import re
 
 class State(Enum):
     RIGHT = auto()
     LEFT = auto()
     FORWARD = auto()
+    BACKWARD = auto()
     IDLE = auto()
     UP = auto()
     DOWN = auto()
@@ -28,25 +30,37 @@ class TrackingControllerNode(Node):
 
         self.turn_speed = 0.5
         self.fwd_speed = 1.0
+        self.tilt_speed = 0.2
 
 
     def _coord_cb(self, msg: String):
         text = msg.data
-        parts = text.replace("x: ", "").replace("y: ", "").split(",")
-        x = int(parts[0].strip())
-        y = int(parts[1].strip())
 
-        dx = x - self.center[0]
-        dy = y - self.center[1]
+        x     = int(re.search(r'x:\s*(\d+)', text).group(1))
+        y     = int(re.search(r'y:\s*(\d+)', text).group(1))
+        area  = int(re.search(r'area:\s*(\d+)', text).group(1))
 
-        if abs(dx) > self.dead_zone:
+        dx = x - self.image_center[0]
+        dy = y - self.image_center[1] 
+
+        self.dead_zone_x = 30   # horizontal 
+        self.dead_zone_y = 60   # vertical 
+
+        # Left/Right
+        if abs(dx) > self.dead_zone_x:
             self.state = State.RIGHT if dx > 0 else State.LEFT
+        #Up/Down
+        elif abs(dy) > self.dead_zone_y:
+            self.state = State.DOWN if dy > 0 else State.UP
+        # Forward/Backward based on bounding box size
+        elif area < 8000:
+            self.state = State.FORWARD   # ball is far, move closer
+        elif area > 20000:
+            self.state = State.BACKWARD  # ball is too close, back up
         else:
             self.state = State.IDLE
 
-        self.get_logger().info(f"x={x} y={y} dx={dx} state={self.state}")
-
-        # can maybe implement forward movement based on size of bounding box
+        self.get_logger().info(f"x={x} y={y} area={area} state={self.state}")
 
     def update(self):
         twist = Twist()
@@ -57,8 +71,14 @@ class TrackingControllerNode(Node):
             twist.angular.z = self.turn_speed   # rotate left
         elif self.state == State.FORWARD:
             twist.linear.x = self.fwd_speed
+        elif self.state == State.BACKWARD:
+            twist.linear.x = -self.fwd_speed
+        elif self.state == State.UP:
+            twist.angular.y = self.tilt_speed   # pitch up
+        elif self.state == State.DOWN:
+            twist.angular.y = -self.tilt_speed  # pitch down
         elif self.state == State.IDLE:
-            pass  # all zeros = stop
+            pass  
 
         self.cmd.publish(twist)
         self.state = State.IDLE  # reset until next coord message
